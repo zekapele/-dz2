f8f54db164d5b58f2ff570bb18dedfbb6a522871
jekapele <zekapele@gmail.com>
2025-03-06 12:57:06 +0200

    Added functionality to convert miles to kilometers and kilometers to miles

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
     // Конвертація миль у кілометри
-   /* public static double milesToKilometers(double miles) {
+   public static double milesToKilometers(double miles) {
         return miles * 1.60934;
     }
 
     // Конвертація кілометрів у милі
     public static double kilometersToMiles(double kilometers) {
         return kilometers / 1.60934;
-    }*/
+    }
 
     public static void main(String[] args) {
-     /*   double miles = 10;
+   double miles = 10;
         double kilometers = 16.0934;
 
         System.out.println(miles + " миль = " + milesToKilometers(miles) + " км");
-        System.out.println(kilometers + " км = " + kilometersToMiles(kilometers) + " миль");*/
-        System.out.println("first commit------infooo");
+        System.out.println(kilometers + " км = " + kilometersToMiles(kilometers) + " миль");
+        // System.out.println("first commit------infooo");
     }
 }
